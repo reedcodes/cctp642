25058438ebd517529e029011c6bf4728d4b4c5b3
Reed Piernock <18013388+reedcodes@users.noreply.github.com>
2021-10-05 14:23:40 -0400

    comment the gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 'use strict';
 
+// This gulp file is mostly for compiling our Sass files using Dart Sass.
 const gulp = require( 'gulp' ),
       sass = require( 'gulp-dart-sass' );
 
+// Look in this directory for any Sass files to compile.
 const cssSource = './source/assets/sass/**/*.scss';
+
+// Include other directories not in this project's source files.
 const cssIncludePaths = [
   './node_modules/reveal.js/css'
 ];
+
+// Send the build files to this directory.
 const cssBuild = './docs/assets/css';
 
 gulp.task( 'sass', function() {
